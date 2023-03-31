d83a79ddad1ea7ee5136c8834317b01cdccf6716
NatiG <nati.g5684@gmail.com>
2023-03-31 03:44:10 +0300

    Remove the form from PopUp & use Input instead

--- src/pages/components/PopUp.js
+++ src/pages/components/PopUp.js
@@ -1,21 +1,9 @@
-export default function PopUp({setAddEvent}) {
-    return (
-        <>
-          <h3>Add a new event</h3>
-          <form>
-            <label for='title'>Title</label>
-            <input type='text' id='title' placeholder="Event Title" required/>
+import InputEvent from "./InputEvent";
 
-            <label for='date'>Date</label>
-            <input type='date' id='date' placeholder='mm/dd/yyyy' required/>
-
-            <label for='description'>Description</label>
-            <input type='text' id='description'/>
-
-            <button type='submit'>Add Event</button>
-          </form>
-
-          <button onClick={() => setAddEvent((prev) => !prev) }>Close</button>
-        </>
-    )
+export default function PopUp({ setAddEvent }) {
+  return (
+    <>
+      <InputEvent setAddEvent={setAddEvent} />
+    </>
+  );
 }
