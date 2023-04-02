8900326bc2d8562b4fcb157ae3109a0621a566ad
NatiG <nati.g5684@gmail.com>
2023-04-02 11:42:50 +0300

    Create a handleSumbit function

--- src/components/InputEvent.js
+++ src/components/InputEvent.js
@@ -1,15 +1,57 @@
-export default function InputEvent({setAddEvent}) {
+import { useRef, useState } from "react";
+
+export default function InputEvent({ setAddEvent, addNewAppointment }) {
+  const [appointmentDetails, setAppointmentDetails] = useState({});
+  const titleRef = useRef();
+  const dateRef = useRef();
+  const descriptionRef = useRef();
+
+  function handleChange() {
+    setAppointmentDetails({
+      title: titleRef.current.value,
+      date: dateRef.current.value,
+      description: descriptionRef.current.value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    addNewAppointment(appointmentDetails);
+    titleRef.current.value = "";
+    dateRef.current.value = "";
+    descriptionRef.current.value = "";
+    setAppointmentDetails("");
+  }
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label for="title">Title</label>
-        <input type="text" id="title" placeholder="Event Title" required />
+        <input
+          type="text"
+          id="title"
+          placeholder="Event Title"
+          ref={titleRef}
+          onChange={handleChange}
+          required
+        />
 
         <label for="date">Date</label>
-        <input type="date" id="date" placeholder="mm/dd/yyyy" required />
+        <input
+          type="date"
+          id="date"
+          placeholder="mm/dd/yyyy"
+          ref={dateRef}
+          onChange={handleChange}
+          required
+        />
 
         <label for="description">Description</label>
-        <input type="text" id="description" />
+        <input
+          type="text"
+          id="description"
+          ref={descriptionRef}
+          onChange={handleChange}
+        />
 
         <button type="submit">Add Event</button>
       </form>
